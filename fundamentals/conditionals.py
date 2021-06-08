7288bb8ca6196d46bc05ab0568d299d537bbde64
David Barcenas <daveepro@outlook.com>
2021-06-07 20:54:19 -0500

    fit converter with conditionals

---
+++ fundamentals/conditionals.py
@@ -0,0 +1,6 @@
+age = int(input('Write your age: '))
+
+if age > 17:
+    print('You are of age')
+else:
+    print('You are a minor')
--- fundamentals/converter.py
+++ fundamentals/converter.py
@@ -1,10 +1,46 @@
-pesos = input('How many pesos do you have?')
-pesos = float(pesos)
+menu = """
+Welcome to the currency converter!
 
-dollar_value = 19.83;
+1 - Mexican pesos
+2 - Argentine pesos 
+3 - Colombian pesos
 
-dollars = pesos / dollar_value;
-dollars = round(dollars, 2)
-dollars = str(dollars)
+Select an option: """
 
-print('You have $' + dollars + ' dollars')
+option = input(menu)
+
+if option == '1':
+    pesos = input('How many mexican pesos do you have? ')
+    pesos = float(pesos)
+
+    dollar_value = 19.83;
+
+    dollars = pesos / dollar_value;
+    dollars = round(dollars, 2)
+    dollars = str(dollars)
+
+    print('You have $' + dollars + ' dollars')
+elif option == '2':
+    pesos = input('How many argentine pesos do you have? ')
+    pesos = float(pesos)
+
+    dollar_value = 64
+
+    dollars = pesos / dollar_value;
+    dollars = round(dollars, 2)
+    dollars = str(dollars)
+
+    print('You have $' + dollars + ' dollars')
+elif option == '3':
+    pesos = input('How many colombian pesos do you have? ')
+    pesos = float(pesos)
+
+    dollar_value = 3875;
+
+    dollars = pesos / dollar_value;
+    dollars = round(dollars, 2)
+    dollars = str(dollars)
+
+    print('You have $' + dollars + ' dollars')
+else:
+    print('Ingrese una opcion correcta')
