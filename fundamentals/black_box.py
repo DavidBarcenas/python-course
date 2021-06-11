0a8c1fe23c88f6d9e823fb67d0d4bc6c39ce18f8
David Barcenas <daveepro@outlook.com>
2021-06-11 17:57:52 -0500

    black box testing

---
+++ fundamentals/black_box.py
@@ -0,0 +1,24 @@
+import unittest
+
+def sum(num1, num2):
+    return num1 + num2
+
+class BlackBoxTest(unittest.TestCase):
+    def test_add_two_positives(self):
+        num_1 = 10
+        num_2 = 5
+
+        result = sum(num_1, num_2)
+        self.assertEqual(result, 15)
+
+    
+    def test_add_two_negatives(self):
+        num_1 = -10
+        num_2 = -7
+
+        result = sum(num_1, num_2)
+        self.assertEqual(result, -17)
+
+
+if __name__ == '__main__':
+    unittest.main()
