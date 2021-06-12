da39922a8ce8711efb35e702da9feb96df1d1f70
David Barcenas <daveepro@outlook.com>
2021-06-12 18:48:00 -0500

    list comprehensions

--- intermediate/lists_dictionaries.py
+++ intermediate/lists_dictionaries.py
@@ -1,4 +1,16 @@
-def run():
+def list_comprehensions():
+    # squares = []
+    # for i in range(1, 101):
+    #     if i % 3 != 0:
+    #         squares.append(i**2)
+
+    squares = [i**2 for i in range(1, 101) if i % 3 != 0]
+    print(squares)
+
+    challenge = [i for i in range(1, 999) if i%4 == 0 and i%6 == 0 and i%9 == 0]
+    print(challenge)
+
+def list_and_dicts():
     my_list = [1, 'Hello', True, 4.5]
     my_dict = {'first_name': 'Dave', 'lastname': 'Barcenas'}
 
@@ -20,6 +32,10 @@ def run():
 
     for item in super_list:
         print(item)
+        
+
+def run():
+    list_comprehensions()    
 
 
 if __name__ == '__main__':
