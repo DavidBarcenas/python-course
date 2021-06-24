21ac7bc75baea22f170776c756fc1cbb32a66782
David Barcenas <daveepro@outlook.com>
2021-06-24 16:54:58 -0500

    use lists and dictionaries - refactor

--- command_line_app/main.py
+++ command_line_app/main.py
@@ -1,40 +1,59 @@
 import sys
 
-clients = ['Pablo', 'Ricardo']
+clients = [
+    {
+        'name': 'Pablo',
+        'company': 'Google',
+        'email': 'pablo@google.com',
+        'position': 'Software engineer'
+    },
+    {
+        'name': 'Raul',
+        'company': 'Facebook',
+        'email': 'raul@facebook.com',
+        'position': 'Data engineer'
+    },
+]
 
-def create_client(client_name):
-    if client_name not in clients:
-        clients.append(client_name)
+
+def create_client(client):
+    if client not in clients:
+        clients.append(client)
+        list_clients()
     else:
         print('The client already exists in the list')
 
 
-def update_client(client_name, update_client_name):
-    if client_name in clients:
-        index = clients.index[client_name]
-        clients[index] = update_client_name 
+def update_client(client_id, update_client):
+    if len(clients) - 1 >= client_id:
+        clients[client_id] = update_client
+        list_clients()
     else:
         _client_not_exist()
 
 
-def delete_client(client_name):
-    if client_name in clients:
-        clients.remove(client_name)
+def delete_client(client_id):
+    if len(clients) - 1 >= client_id:
+        clients.pop(client_id)
+        list_clients()
     else:
         _client_not_exist()
 
 
 def search_client(client_name):   
     for client in clients:
-        if client != client_name:
+        if client['name'] != client_name:
             continue
         else:
             return True
 
 
 def list_clients():
+    print('uid |  name  | company  | email  | position ')
+    print('*' *50)
+
     for idx, client in enumerate(clients):
-        print(f'{idx}: {client}')
+        print(f'{idx} | {client["name"]} | {client["company"]} | {client["email"]} | {client["position"]}')
 
 
 def _print_welcome():
@@ -47,24 +66,35 @@ def _print_welcome():
     print('[S]earch client')
 
 
+def _get_data_from_user():
+    client = {
+        'name': _get_client_field('name'),
+        'company': _get_client_field('company'),
+        'email': _get_client_field('email'),
+        'position': _get_client_field('position'),
+    }
+
+    return client
+
+
 def _client_not_exist():
     print('The client does not exist')
 
 
-def _get_client_name():
-    client_name = None
+def _get_client_field(field_name):
+    field = None
 
-    while not client_name:
-        client_name = input('What is the client name? ')
+    while not field:
+        field = input(f'What is the client {field_name}? ')
 
-        if client_name == 'exit':
-            client_name = None
-            break;
+        if field_name == 'exit':
+            field = None
+            break
 
-    if not client_name:
+    if not field:
         sys.exit()
 
-    return client_name
+    return field;
 
 
 if __name__ == '__main__':
@@ -74,20 +104,17 @@ if __name__ == '__main__':
     command = command.upper()
 
     if command == 'C':
-        client_name = _get_client_name()
-        create_client(client_name)
-        list_clients()
-    elif command == 'D':
-        client_name = _get_client_name()
-        delete_client(client_name)
-        list_clients()
+        client = _get_data_from_user()
+        create_client(client)
     elif command == 'U':
-        client_name = _get_client_name()
-        update_client_name = input('What is the updated client name? ')
-        update_client(client_name,update_client_name)
-        list_clients()
+        client_id = int(_get_client_field('id'))
+        client_data = _get_data_from_user()
+        update_client(client_id, client_data)
+    elif command == 'D':
+        client_id = int(_get_client_field('id'))
+        delete_client(client_id)
     elif command == 'S':
-        client_name = _get_client_name()
+        client_name = _get_client_field('name')
         found = search_client(client_name)
         
         if found:
