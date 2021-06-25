958f49779f0400480101337b3013a552631c431c
David Barcenas <daveepro@outlook.com>
2021-06-25 15:27:12 -0500

    create models and services

--- command_line_app/clients/commands.py
+++ command_line_app/clients/commands.py
@@ -1,29 +1,39 @@
 import click
+from clients.services import ClientService
+from clients.models import Client
 
 @click.group()
 def clients():
     """Manages the clients lifecycle"""
 
 @clients.command()
-@click.pass_context()
+@click.option('-n', '--name', type=str, prompt=True, help='The client name')
+@click.option('-c', '--company', type=str, prompt=True, help='The client company')
+@click.option('-e', '--email', type=str, prompt=True, help='The client email')
+@click.option('-p', '--position', type=str, prompt=True, help='The client position')
+@click.pass_context
 def create(ctx, name, company, email, position):
     """Create a new client"""
+    client = Client(name, company, email, position)
+    client_service = ClientService(ctx.obj['clients_table'])
+
+    client_service.create_client(client)
 
 
 @clients.command()
-@click.pass_context()
+@click.pass_context
 def list(ctx):
     """List all clients"""
 
 
 @clients.command()
-@click.pass_context()
+@click.pass_context
 def update(ctx, client_uid):
     """Update a client"""
 
 
 @clients.command()
-@click.pass_context()
+@click.pass_context
 def delete(ctx, client_uid):
     """Delete a client"""
 
