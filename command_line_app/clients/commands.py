d68674f052e86db27003896725ead40e2ccff6f0
David Barcenas <daveepro@outlook.com>
2021-06-25 16:10:20 -0500

    finish list and update methods

--- command_line_app/clients/commands.py
+++ command_line_app/clients/commands.py
@@ -16,7 +16,6 @@ def create(ctx, name, company, email, position):
     """Create a new client"""
     client = Client(name, company, email, position)
     client_service = ClientService(ctx.obj['clients_table'])
-
     client_service.create_client(client)
 
 
@@ -24,12 +23,32 @@ def create(ctx, name, company, email, position):
 @click.pass_context
 def list(ctx):
     """List all clients"""
+    client_service = ClientService(ctx.obj['clients_table'])
+    clients_list = client_service.list_clients()
+
+    click.echo('  ID  |  NAME  |  COMPANY  |  EMAIL  |  POSITION ')
+    click.echo('*' * 50)
+    
+    for client in clients_list:
+        click.echo(f"{client['uid']} | {client['name']} | {client['company']} | {client['email']} | {client['position']}")
 
 
 @clients.command()
+@click.argument('client_uid')
 @click.pass_context
 def update(ctx, client_uid):
     """Update a client"""
+    client_service = ClientService(ctx.obj['clients_table'])
+    client_list = client_service.list_clients()
+    client = [client for client in client_list if client['uid'] == client_uid]
+
+    if client:
+        client = _update_client_flow(Client(**client[0]))
+        client_service.update_client(client)
+
+        click.echo('Client updated')
+    else:
+        click.echo('Client not found')
 
 
 @clients.command()
@@ -37,5 +56,15 @@ def update(ctx, client_uid):
 def delete(ctx, client_uid):
     """Delete a client"""
 
+
+def _update_client_flow(client):
+    click.echo('Leave empty if you dont want to modify the value')
+
+    client.name = click.prompt('New name', type=str, default=client.name)
+    client.company = click.prompt('New company', type=str, default=client.name)
+    client.email = click.prompt('New email', type=str, default=client.email)
+    client.position = click.prompt('New position', type=str, default=client.position)
+
+    return client
 
 all = clients
